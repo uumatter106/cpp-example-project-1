4db78746bce8ef90e4b504de0fecfda7e2eb6c47
Burak Enes Uğurlu <burakenes_1311@hotmail.com>
2021-07-27 16:26:51 +0300

    A new part `part22` fun added that is very similar to `part21`
    * `part2` fun renamed aka `part21`
    * Forward declarations updated.
    ! There is a `part22();` statement in `main` fun. This was forgotten.
        Will be deleted in a new commit hopely.

--- lib/main.cpp
+++ lib/main.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 
 void part1();
-void part2();
+void part21();
+void part22();
 
 int main() {
+    part22();
 }
 
 // 'Welcome' message printing
@@ -19,7 +21,7 @@ void part1() {
 }
 
 // Sum of two numbers.
-void part2() {
+void part21() {
     // Defining and assigning variables('number1', 'number2', 'sum') with zero as initial value.
     int number1 = 0;
     int number2 = 0;
@@ -38,4 +40,22 @@ void part2() {
 
     // Say the result.
     std::cout << "Sum is " << sum << std::endl;
+}
+
+// Sum of two numbers.
+void part22() {
+    // Defining and assigning variables('number1', 'number2') with zero as initial value via chained assignment processing.
+    //! Caution: This chainment is a bad way for assigning. In that way, you can't comment properly what they are named or used for.
+    int number1 = 0, number2 = 0;
+
+    // We want to enter a integer number from user firstly.
+    std::cout << "Enter first integer: ";
+    std::cin >> number1;
+
+    // We want to enter another integer number after that.
+    std::cout << "Enter second integer: ";
+    std::cin >> number2;
+
+    // Sum them all and say the result on the same line.
+    std::cout << "Sum is " << number1 + number2 << std::endl;
 }
